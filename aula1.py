68e68958ce43ae6697218f4a4cd63b9f29c04660
bea2071 <96319941+bea2071@users.noreply.github.com>
2024-05-18 17:59:48 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,34 @@
+n = input('Digite um numero')
+
+dicti = {
+'numero' : 10,
+'numero2' :20
+}
+
+dicti['numero'] = n
+print(dicti)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# pedido  =  input('>>')
+# valor  =  10
+# dicionario ={'calça': 50.00 , 'camiseta': 45.00, 'meias': 20.00 }
+# dicionario[pedido] = float(valor)
+# print('Seu carrinho: ', dicionario)
+
+# soma = 0
+# for chave, valor in dicionario.items():
+#     print(chave, ':', valor )
+#     soma = soma + valor
+
+# print('Total, R$: ', soma) 
